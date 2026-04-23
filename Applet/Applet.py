21b773952de9beb1bb11276acd9c4312135da103
ksenia <ksvkovach@gmail.com>
2026-04-23 12:17:49 -0400

    accepted changes

--- Applet/Applet.py
+++ Applet/Applet.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import streamlit as st
 import numpy as np
 import pandas as pd
-=======
 # import any libraries that we need
 
 # get input parameters from user
@@ -13,4 +11,3 @@ import pandas as pd
 # sends those values to the global steady state function
 # returned values get used to make a graph 
 
->>>>>>> d6badf2d2c511922b577fada9f0f01acc2fa0800
