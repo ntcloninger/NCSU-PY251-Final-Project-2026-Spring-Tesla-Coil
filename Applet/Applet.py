cb1250e6405ca35e44896c4151adab7cd81c892e
ksenia <ksvkovach@gmail.com>
2026-04-23 12:29:31 -0400

    updated applet to include more imports for global function creation

--- Applet/Applet.py
+++ Applet/Applet.py
@@ -1,12 +1,24 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from Input_Wrapper import input_wrapper
+from Steady_State_Driver import # put in the function 
+from System_iterator import Tesla_Coil_Solver
+
 # import any libraries that we need
 
 # get input parameters from user
 
+C_1 = # put in user's input for C1
+C_2 = # put in user's input for C2
+sparky_distance = # put in user's input for sparky's distance
+ac_amp = # put in user's input for ac amplitude
+ac_freq = # put n user's input input for ac frequency 
+
 # calls global wrapper function, plugging in those input parameters
 # puts the return values into their own variables
+
+
 
 # sends those values to the global steady state function
 # returned values get used to make a graph 
