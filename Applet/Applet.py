d6badf2d2c511922b577fada9f0f01acc2fa0800
ksenia <ksvkovach@gmail.com>
2026-04-23 12:00:22 -0400

    code outline for applet

---
+++ Applet/Applet.py
@@ -0,0 +1,10 @@
+# import any libraries that we need
+
+# get input parameters from user
+
+# calls global wrapper function, plugging in those input parameters
+# puts the return values into their own variables
+
+# sends those values to the global steady state function
+# returned values get used to make a graph 
+
