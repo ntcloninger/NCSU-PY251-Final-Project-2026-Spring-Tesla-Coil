c52d4e6032a816b292e7e188a4ae789abf5f8cc5
ksenia <ksvkovach@gmail.com>
2026-04-23 12:50:17 -0400

    applet changes

--- Applet/Applet.py
+++ Applet/Applet.py
@@ -7,17 +7,19 @@ from System_iterator import Tesla_Coil_Solver
 
 # import any libraries that we need
 
-# get input parameters from user
+# get input parameters from user and assigns those values to their corresponding value
 
-C_1 = # put in user's input for C1
-C_2 = # put in user's input for C2
+
+C1 = # put in user's input for C1
+C2 = # put in user's input for C2
 sparky_distance = # put in user's input for sparky's distance
 ac_amp = # put in user's input for ac amplitude
 ac_freq = # put n user's input input for ac frequency 
 
+
 # calls global wrapper function, plugging in those input parameters
 # puts the return values into their own variables
-
+init_state, pars = input_wrapper(C_1=C1 )
 
 
 # sends those values to the global steady state function
