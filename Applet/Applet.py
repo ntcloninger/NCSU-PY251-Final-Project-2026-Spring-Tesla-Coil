4812913158d5ecb3b82020f265f9d5c12731d793
AlexEskridge <sgepotato@gmail.com>
2026-04-25 20:33:09 -0400

    Submit Button and Graph

--- Applet/Applet.py
+++ Applet/Applet.py
@@ -5,29 +5,38 @@ from Tesla_Coil_Code.Steady_State.Input_Wrapper import input_wrapper
 from Tesla_Coil_Code.Steady_State.Graph_Creater import Cycle_Graph_20
 from Tesla_Coil_Code.Tesla_Coil.TeslaCoil import Tesla_Coil_Solver
 
-# import any libraries that we need
-
-
-
-# Shows the tesla coil curcuit for demonstration purposes 
 st.image("1000002237.jpg", caption="Tesla Coil Circuit Diagram")
 
-# get input parameters from user and assigns those values to their corresponding value
-C1 = st.number_input("Type C1 Value[F]: ", value=1)
-C2 = st.number_input("Type C2 Value[F]: ",value=10)
-C3 = st.number_input("Type C3 Value[F]: ",value=100)
-R1 = st.number_input("Type R1 Value[Ohm]: ", value=10)
-R2 = st.number_input("Type R2 Value[Ohm]: ", value=20)
-R3 = st.number_input("Type R3 Value[Ohm]: ", value=100)
-L1 = st.number_input("Type L1 Value[H]: ", value=1)
-L2 = st.number_input("Type L2 Value[H]: ", value=50)
-L3 = st.number_input("Type L3 Value[H]: ", value=10)
-L4 = st.number_input("Type L4 Value[H]: ", value=100)
-k1 = st.number_input("Type k1 Value[F]: ",value=1)
-k2 = st.number_input("Type k1 Value[F]: ",value=0.5)
-sparky = st.number_input("Spark Gap Distance Value[m]: ",value=0.1)
-ac_amp = st.number_input("AC Amplitude Value[m]: ",value=170)
-ac_freq = st.number_input("AC Frequency Value[Hz]: ",value=60)
+with st.form("Calc_Form"):
+    st.header("Input Parameters")
+
+    C1 = st.number_input("Type C1 Value[F]: ", value=1)
+    C2 = st.number_input("Type C2 Value[F]: ",value=100)
+    C3 = st.number_input("Type C3 Value[F]: ",value=100)
+    R1 = st.number_input("Type R1 Value[Ohm]: ", value=10)
+    R2 = st.number_input("Type R2 Value[Ohm]: ", value=20)
+    R3 = st.number_input("Type R3 Value[Ohm]: ", value=100)
+    L1 = st.number_input("Type L1 Value[H]: ", value=1)
+    L2 = st.number_input("Type L2 Value[H]: ", value=50)
+    L3 = st.number_input("Type L3 Value[H]: ", value=10)
+    L4 = st.number_input("Type L4 Value[H]: ", value=100)
+    k1 = st.number_input("Type k1 Value[F]: ",value=1)
+    k2 = st.number_input("Type k1 Value[F]: ",value=0.5)
+    sparky_distance = st.number_input("Spark Gap Distance Value[m]: ",value=0.1)
+    ac_amp = st.number_input("AC Amplitude Value[m]: ",value=170)
+    ac_freq = st.number_input("AC Frequency Value[Hz]: ",value=60)
+
+    submitted = st.form_submit_button("Upload and Run")
+
+if submitted:
+    st.success("Values uploaded!")
+
+t_array = np.linspace(0, 10, 20)
+y_array = 2 * t_array
+
+chart_data = pd.DataFrame(y_array, index=t_array)
+
+st.line_chart(chart_data)
 
 # calls global wrapper function, plugging in those input parameters
 # puts the return values into their own variables
