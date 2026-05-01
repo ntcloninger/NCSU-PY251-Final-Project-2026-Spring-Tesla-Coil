9bb1988d7e38955d852de76a574d6b9cfe59bb67
ksenia <ksvkovach@gmail.com>
2026-05-01 16:47:15 -0400

    we did this (ksenia and alex)

--- CircuitTester.py
+++ CircuitTester.py
@@ -6,7 +6,6 @@ import pandas as pd
 import schemdraw
 import schemdraw.elements as e
 
-#https://schemdraw.readthedocs.io/en/stable/elements/electrical.html
 #Background Color
 st.markdown(
     """
@@ -20,55 +19,39 @@ st.markdown(
 )
 
 # Shows the tesla coil circuit for demonstration purposes 
-st.image("1000002237.jpg", caption="Tesla Coil Circuit Diagram")
-
-# Create a simple RC circuit
 with schemdraw.Drawing() as d:
 
     d.config(unit=2.2)
 
     #Power supply
-    d += e.SourceSin().at((0,0)).label("AC")
-    d+= e.Line().right().length(1.5)
+    d += e.SourceSin().at((0,0))
+    d += e.Dot().label("Node A")
+    d += e.Line().right().length(1.5)
     #Add the transformer
-    d += e.Transformer(t1=4, t2=4).anchor("p1")
-    d += e.Line().at((1.5,0)).to((1.5, -2))
-    d += e.Line().at((0, -2)).to((1.5, -2))
-    d += e.Line().at((0, -2)).up().length(2)
+    d += e.Transformer(t1=10, t2=10).anchor("p1")
+    d += e.Line().at((0, -1.8)).to((1.5, -1.8))
+    d += e.Line().at((0, -1.8)).up().length(2)
 
     #Start the second part of the loop
-    d += e.Line().at((4.5, 0.8)).right().length(1.5)
-    d += e.Capacitor().right()
+    d += e.Line().at((2.5, 2.2)).right().length(1.5)
+    d += e.Dot().label("Node B")
     d += e.Line().right().length(1)
+    d += e.Capacitor().right()
+    
     #Spark gap
-    d += e.SparkGap().down()
-    d += e.Line().down().length(1)
+    d += e.Line().at((2.5, -1.8)).right().length(1.5)
+    d += e.SparkGap().up().length(4)
+    d += e.Line().at((4,-1.8)).right().length(3.2)
     #Coil
-    d += e.Inductor2().left()
-    d += e.Line().left().length(1)
-    d += e.Line().up().to((4.5, -0.8))
+    d += e.Inductor2().up()
+    d += e.Line().at((7.2,0)).length(2.2)
 
     #Right side
-    d += e.Inductor().at((10, -1.5)).up().length(4)
-    d += e.Line().at((10, -1.5)).down().length(1)
+    d += e.Inductor().at((8, 2.6)).down().length(4)
     d += e.Ground()
     #Torus
-    d += e.Line().at((10, 2.5)).up().length(0.5)
-    d += e.Dot()
-
-
-    #creates all of the parts of the circuit
-    """ source = d.add(e.SourceSin().down())
-    d += d.add(e.Line().right()) 
-    tr = d.add(e.Transformer().right())
-    d += d.add(e.Line().right()) """
-
-    #d += d.add(e.SparkGap().right())
-    #d += d.add(e.Capacitor().up())
-    #d += d.add(e.Inductor().right())
-    #d += d.add(e.Ground().down())
-    #d += d.add(e.Inductor().up())
-    #d += d.add(e.AntennaLoop().up())
+    d += e.AntennaLoop().at((8,2.6))
+    d += e.Dot().label("Node C").at((8.1,4))
 
     # Save to file
     d.save('circuit.png')
