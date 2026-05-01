3793153334f6974ed96d6c18097ca4fd4a20d120
Nathan Cloninger <ntclonin@ncsu.edu>
2026-05-01 17:44:48 -0400

    notes

--- Tesla_Coil_Code/TeslaCoil.py
+++ Tesla_Coil_Code/TeslaCoil.py
@@ -4,7 +4,7 @@ from scipy.integrate import odeint
 def Tesla_Coil_Solver(t, pars): #take in initial value and parameters
 
     class Tesla_Coil:
-        def __init__(self, pars):
+        def __init__(self, pars): #set paramters, Mi, and breakdown voltage
             self.L1 = pars[0]
             self.L2 = pars[1]
             self.L3 = pars[2]
@@ -39,13 +39,13 @@ def Tesla_Coil_Solver(t, pars): #take in initial value and parameters
 
             #q[i-1][derivative]
 
-        def f(self,t):
+        def f(self,t): #driving functions
             return self.AC_amplitude*np.sin(self.AC_frequency*t)
         
-        def df(self,t):
+        def df(self,t): #derivative driving function
             return self.AC_frequency*self.AC_amplitude*np.cos(self.AC_frequency*t)
 
-        def deriv1(self, q,t):#organizes derivative equations with given initial
+        def deriv1(self, q,t):#organizes derivative equations with given initial before breakdown
             dq2_2_temp = (1/(self.L1*self.L4*(self.L2*(self.k1**2-1)+self.L3*(self.k2**2-1))))\
                 *(self.L4*(self.AC_amplitude*self.AC_frequency*np.cos(self.AC_frequency*t) + q[1]/self.C1 + self.R1*q[2])\
                   +self.L1*(self.R3*q[8] + q[7]/self.C3) + self.L1*self.L4*(self.R2*q[5]+q[4]/self.C2))
@@ -62,7 +62,7 @@ def Tesla_Coil_Solver(t, pars): #take in initial value and parameters
                     dq3_2_temp]
             return dq
         
-        def deriv2(self, q,t):#organizes derivative equations with given initial
+        def deriv2(self, q,t):#organizes derivative equations with given initial after breakdown
             dq2_2_temp = (self.M2/(self.L4*(self.L4+self.L3*(1-self.k2**2))))*(-self.R3*q[8]-(1/self.C2)*q[7])
             dq1_2_temp = (-self.df(t) - q[1]/self.C1 - self.R1*q[2])/self.L1
             dq3_2_temp = (-self.R3*q[8] - q[7]/self.C3 + self.M2*dq2_2_temp)/self.L4
@@ -86,4 +86,4 @@ def Tesla_Coil_Solver(t, pars): #take in initial value and parameters
     q1 = diffeq_solver_from_scipy(input_system.deriv1, input_system.q_initial_1, t)
     qi2 = [q1[-1][0],q1[-1][1],q1[-1][2],input_system.Breakdown_Voltage*input_system.C2,q1[-1][4],q1[-1][5],q1[-1][6],q1[-1][7],q1[-1][8]]
     q2 = diffeq_solver_from_scipy(input_system.deriv2, qi2, t + t[-1]) 
-    return q1,q2
+    return q1,q2 #returns before and after breakdown
