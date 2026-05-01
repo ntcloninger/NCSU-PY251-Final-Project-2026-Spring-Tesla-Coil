cea25985eabef86b298f1ca2202a106d980e702f
ksenia <ksvkovach@gmail.com>
2026-05-01 15:59:07 -0400

    i did this (Alex)

--- CircuitTester.py
+++ CircuitTester.py
@@ -25,11 +25,43 @@ st.image("1000002237.jpg", caption="Tesla Coil Circuit Diagram")
 # Create a simple RC circuit
 with schemdraw.Drawing() as d:
 
+    d.config(unit=2.2)
+
+    #Power supply
+    d += e.SourceSin().at((0,0)).label("AC")
+    d+= e.Line().right().length(1.5)
+    #Add the transformer
+    d += e.Transformer(t1=4, t2=4).anchor("p1")
+    d += e.Line().at((1.5,0)).to((1.5, -2))
+    d += e.Line().at((0, -2)).to((1.5, -2))
+    d += e.Line().at((0, -2)).up().length(2)
+
+    #Start the second part of the loop
+    d += e.Line().at((4.5, 0.8)).right().length(1.5)
+    d += e.Capacitor().right()
+    d += e.Line().right().length(1)
+    #Spark gap
+    d += e.SparkGap().down()
+    d += e.Line().down().length(1)
+    #Coil
+    d += e.Inductor2().left()
+    d += e.Line().left().length(1)
+    d += e.Line().up().to((4.5, -0.8))
+
+    #Right side
+    d += e.Inductor().at((10, -1.5)).up().length(4)
+    d += e.Line().at((10, -1.5)).down().length(1)
+    d += e.Ground()
+    #Torus
+    d += e.Line().at((10, 2.5)).up().length(0.5)
+    d += e.Dot()
+
+
     #creates all of the parts of the circuit
-    source = d.add(e.SourceSin().up())
-    tr = d.add(e.Transformer().down())
-    d += d.add(e.Line().right().at(source.start).to(tr.p1)) 
-    d += d.add(e.Line().left().at(source.end).to(tr.p2))
+    """ source = d.add(e.SourceSin().down())
+    d += d.add(e.Line().right()) 
+    tr = d.add(e.Transformer().right())
+    d += d.add(e.Line().right()) """
 
     #d += d.add(e.SparkGap().right())
     #d += d.add(e.Capacitor().up())
